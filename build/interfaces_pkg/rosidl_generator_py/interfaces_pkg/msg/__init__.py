c515ecf417f700a8a256289e3be50b1407590de1
Bruno Silva <brunogdpa7@gmail.com>
2025-01-17 10:46:10 +0100

    updated paths to generic

--- build/interfaces_pkg/rosidl_generator_py/interfaces_pkg/msg/__init__.py
+++
@@ -1,2 +0,0 @@
-from interfaces_pkg.msg._detected_object import DetectedObject  # noqa: F401
-from interfaces_pkg.msg._yolov8_objects import Yolov8Objects  # noqa: F401
--- build/obj_det/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/bruno/umib_sam2_yolov8_ros2_ws/install/obj_det'
