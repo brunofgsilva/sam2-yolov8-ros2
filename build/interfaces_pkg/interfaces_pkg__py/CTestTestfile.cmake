c497a94044b71db277fe4f1323ea4ff1d86cb40d
Bruno Silva <brunogdpa7@gmail.com>
2025-01-17 16:05:32 +0100

    solving some sam2 bugs

--- build/interfaces_pkg/interfaces_pkg__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bruno/umib_sam2_yolov8_ros2_ws/build/interfaces_pkg/interfaces_pkg__py
-# Build directory: /home/bruno/umib_sam2_yolov8_ros2_ws/build/interfaces_pkg/interfaces_pkg__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- install/interfaces_pkg/share/interfaces_pkg/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(interfaces_pkg_IDL_FILES "msg/DetectedObject.idl;msg/Yolov8Objects.idl")
-set(interfaces_pkg_INTERFACE_FILES "msg/DetectedObject.msg;msg/Yolov8Objects.msg")
